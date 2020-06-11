d0ecc97fb624366af3805add1ae5ae24c6d4597b
Chris Young <chris@chris.net.au>
2020-06-11 14:34:13 +1000

    Cleanup G601: Implicit memory aliasing in for loop.
    Fairly sure this is a false positive designed to catch instances where
    the loop causes the alias to be overwritten before use.

--- cmd/trpc/add.go
+++ cmd/trpc/add.go
@@ -17,6 +17,8 @@ type AddOptions struct {
 func Add(c *Command) {
 	opts, ok := c.CommandOptions.(AddOptions)
 	optionsCheck(ok)
+	// This silences G601: Implicit memory aliasing in for loop. (gosec)
+	var argCopy string
 	for _, arg := range c.PositionalArgs {
 		var torrent *transmissionrpc.Torrent
 
@@ -29,16 +31,16 @@ func Add(c *Command) {
 		// Assume it's a file.
 		if err != nil || url.Scheme == "" {
 			fmt.Println("Treating as file.")
-			filepath := arg
 			var b64 string
-			b64, err = transmissionrpc.File2Base64(filepath)
+			b64, err = transmissionrpc.File2Base64(arg)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "can't encode '%s' content as base64: %v", filepath, err)
+				fmt.Fprintf(os.Stderr, "can't encode '%s' content as base64: %v", arg, err)
 				return
 			}
 			payload.MetaInfo = &b64
 		} else { // It's a URL, pass it to transmission.
-			payload.Filename = &arg
+			argCopy = arg
+			payload.Filename = &argCopy
 		}
 		if !c.CommonOptions.DryRun {
 			torrent, err = c.Client.TorrentAdd(&payload)
